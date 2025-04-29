c54d0ac25396c9c0c2ca7f9237f9da30fe0f946e
diegoramoscagepa <diego.ramos@cagepa.pb.gov.br>
2025-04-29 10:00:36 -0300

    Iniciando projeto

---
+++ 01_introducao/api_de_composicao.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref, onMounted, onUpdated } from 'vue'
+
+// reactive state
+const count = ref(0)
+
+// functions that mutate state and trigger updates
+function increment() {
+    count.value++
+}
+
+// Serve para montar o componente
+onMounted(() => console.log(`The initial count is ${count.value}.`));
+
+
+</script>
+
+<template>
+    <button @click="increment">Count is: {{ count }}</button>
+</template>
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # course-vue-js
 Course about vuejs
+
+###  Iniciar projeto vue-js
+npm create vue@latest <my-project-vue>
+cd <my-project-vue>
+npm install
+npm run dev
+
+### Guia de uso TypeScript

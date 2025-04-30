40f77383016a47d4c834a092a8bdc5c992ea7966
diegoramoscagepa <diego.ramos@cagepa.pb.gov.br>
2025-04-30 14:43:09 -0300

    app e index

--- 02_criando_projeto/raiz_in_dom/app.js
+++ 02_criando_projeto/raiz_in_dom/app.js
@@ -1,15 +1,26 @@
+// Acessamos Vue do objeto global (quando usando CDN)
+const { createApp } = Vue;
+
+// Criamos a aplicação Vue
 const app = createApp({
-    data() {
-      return {
-        count: 0
-      }
-    },
-    methods: {
-      increment() {
-        this.count++;
-        if (this.count === 10) {
-          alert('Você atingiu 10 cliques!');
-        }
+  data() {
+    return {
+      count: 0,
+      buttonText: 'Clique-me'
+    }
+  },
+  methods: {
+    increment() {
+      this.count++;
+      if (this.count === 5) {
+        this.buttonText = 'Continue clicando';
       }
     }
-  });
+  },
+  mounted() {
+    console.log('Aplicação Vue montada!');
+  }
+});
+
+// Montamos no elemento #app
+app.mount('#app');
